32b235e0fc7ea1cfda0c7e606472a45e7a959bb7
Emilly <110494320+Emillyzinha@users.noreply.github.com>
2023-03-28 22:55:59 -0300

    Update nav.jsx

--- nav.jsx
+++ nav.jsx
@@ -4,14 +4,16 @@ import logo from "../../assets/logo.png"
 function Nav() {
     return (
         <>
-            <div className="flex h-24 border-b-2 border-slate-800">
-                <img className="max-h-8 ml-24 mt-8" src={logo} />
-                <h1 className="font-titulo-nav text-3xl text-verde-nav p-7">Loja</h1>
-                <h1 className="font-titulo-nav text-3xl text-verde-nav p-7">Artigos</h1>
-                <h1 className="font-titulo-nav text-3xl text-verde-nav p-7">Suporte</h1>
-            </div>
-            <div>
-                <h1 className="font-titulo-nav text-3xl text-verde-nav p-7">Minha conta</h1>
+            <div className="flex place-content-between border-b-2 border-slate-800">
+                <div className="flex h-24">
+                    <img className="max-h-8 ml-24 mt-8" src={logo} />
+                    <h1 className="font-titulo-nav text-3xl text-verde-nav p-7">Loja</h1>
+                    <h1 className="font-titulo-nav text-3xl text-verde-nav p-7">Artigos</h1>
+                    <h1 className="font-titulo-nav text-3xl text-verde-nav p-7">Suporte</h1>
+                </div>
+                <div>
+                    <h1 className="font-titulo-nav text-3xl text-verde-nav p-7 mr-8">Minha conta</h1>
+                </div>
             </div>
         </>
     )
